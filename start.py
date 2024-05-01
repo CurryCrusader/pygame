34f01fabd3f73aa46d5d1270b3229710e28472c4
CurryCrusader <152342239+CurryCrusader@users.noreply.github.com>
2024-05-01 19:17:25 -0400

    Delete start.py

--- start.py
+++
@@ -1,9 +0,0 @@
-import pygame
-def startscreen():
-    startImage = 0
-    startRect = 0
-    start = True
-    while start:
-        startImage = pygame.image.load("start.jpg")
-        startImage = pygame.transform.scale(startImage, (500, 500))
-        startRect = startImage.get_rect()
